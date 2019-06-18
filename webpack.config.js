259a0803a861d91ed3e2ad112f31fa46dc35b5d1
Edmund Chang <chang.edmund@gmail.com>
2019-06-18 16:20:01 -0700

    updating dependencies and links

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
     filename: 'bundle.js',
   },
 
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
